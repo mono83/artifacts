2154d049ecde8e100a73aa60ac6294e66298d006
Gotterdemarung <gotterdemarung@gmail.com>
2022-08-01 22:08:30 +0300

    :sparkles: Results table

--- data/resultsTable.go
+++ data/resultsTable.go
@@ -1,6 +1,8 @@
 package data
 
-import "sort"
+import (
+	"sort"
+)
 
 // ResultsRow contains information on single row of data
 type ResultsRow struct {
@@ -8,9 +10,9 @@ type ResultsRow struct {
 	Value  int64
 }
 
-// Without column returns row with one group column removed
+// WithoutColumn returns row with one group column removed
 func (r ResultsRow) WithoutColumn(c int) ResultsRow {
-	return ResultsRow{Value: r.Value, Groups: remove(r.Groups, c)}
+	return ResultsRow{Value: r.Value, Groups: sliceRemove(r.Groups, c)}
 }
 
 // SameGroups return true if current and other results rows
@@ -39,7 +41,7 @@ func (r *ResultsTable) Add(value int64, groups ...string) {
 	r.Rows = append(r.Rows, ResultsRow{Value: value, Groups: groups})
 }
 
-// Without column returns results table with one column removed
+// WithoutColumn returns results table with one column removed
 func (r ResultsTable) WithoutColumn(c int) ResultsTable {
 	// Rebuilding group values
 	var rows []ResultsRow
@@ -49,30 +51,20 @@ func (r ResultsTable) WithoutColumn(c int) ResultsTable {
 
 	return ResultsTable{
 		Metric: r.Metric,
-		Groups: remove(r.Groups, c),
+		Groups: sliceRemove(r.Groups, c),
 		Rows:   rows,
 	}
 }
 
 // SortByGroupValues performs table data sorting by group values
 func (r *ResultsTable) SortByGroupValues() {
-	sort.Slice(r.Rows, func(i, j int) bool {
-		ri, rj := r.Rows[i].Groups, r.Rows[j].Groups
-		for k, v := range ri {
-			if v < rj[k] {
-				return true
-			}
-		}
-		return false
-	})
+	sort.Sort(r)
 }
 
 // Recombine recombines column data
-func (r *ResultsTable) Recombine() ResultsTable {
-	// Sorting
-	r.SortByGroupValues()
-
-	return r.recombineSorted()
+func (r *ResultsTable) Recombine() []ResultsTable {
+	var skip [][]string
+	return r.recombineSorted(&skip)
 }
 
 func (r ResultsTable) mergeSorted() ResultsTable {
@@ -91,15 +83,61 @@ func (r ResultsTable) mergeSorted() ResultsTable {
 	}
 }
 
-func (r ResultsTable) recombineSorted() ResultsTable {
+func (r *ResultsTable) recombineSorted(skip *[][]string) (out []ResultsTable) {
 	for x := 0; x < len(r.Groups); x++ {
 		truncated := r.WithoutColumn(x)
-
+		todo := true
+		for _, s := range *skip {
+			if sliceEquals(s, truncated.Groups) {
+				todo = false
+				break
+			}
+		}
+		if todo {
+			*skip = append(*skip, truncated.Groups)
+			truncated.SortByGroupValues()
+			merged := truncated.mergeSorted()
+			out = append(out, merged)
+			if len(merged.Groups) > 0 {
+				out = append(out, merged.recombineSorted(skip)...)
+			}
+		}
 	}
+	return
 }
 
-func remove(slice []string, s int) (out []string) {
+// Len is sort.Interface implementation
+func (r ResultsTable) Len() int { return len(r.Rows) }
+
+// Swap is sort.Interface implementation
+func (r *ResultsTable) Swap(i, j int) { r.Rows[i], r.Rows[j] = r.Rows[j], r.Rows[i] }
+
+// Less is sort.Interface implementation
+func (r *ResultsTable) Less(i, j int) bool {
+	ri, rj := r.Rows[i].Groups, r.Rows[j].Groups
+	for k := range ri {
+		if ri[k] == rj[k] {
+			continue
+		}
+		return ri[k] < rj[k]
+	}
+	return false
+}
+
+func sliceRemove(slice []string, s int) (out []string) {
 	out = append(out, slice[:s]...)
 	out = append(out, slice[s+1:]...)
 	return
+}
+
+func sliceEquals(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
 }
--- data/resultsTable_test.go
+++ data/resultsTable_test.go
@@ -32,3 +32,89 @@ func TestResultsTableWithoutColumn(t *testing.T) {
 		assert.Equal(t, int64(614), r1.Rows[1].Value)
 	}
 }
+
+func TestSortByGroupValues(t *testing.T) {
+	r := ResultsTable{Metric: "foo", Groups: []string{"Gate", "Status", "Enabled"}}
+	r.Add(5, "1", "activated", "true")
+	r.Add(3, "1", "in_progress", "true")
+	r.Add(8, "1", "activated", "false")
+	r.Add(2, "2", "investigation", "true")
+	r.Add(2, "2", "activated", "true")
+	r.SortByGroupValues()
+
+	assert.Equal(t, []string{"1", "activated", "false"}, r.Rows[0].Groups)
+	assert.Equal(t, []string{"1", "activated", "true"}, r.Rows[1].Groups)
+	assert.Equal(t, []string{"1", "in_progress", "true"}, r.Rows[2].Groups)
+	assert.Equal(t, []string{"2", "activated", "true"}, r.Rows[3].Groups)
+	assert.Equal(t, []string{"2", "investigation", "true"}, r.Rows[4].Groups)
+}
+
+func TestMergeSorted(t *testing.T) {
+	r := ResultsTable{Metric: "foo", Groups: []string{"A"}}
+	r.Add(23, "bar")
+	r.Add(1, "bar")
+	r.Add(10, "foo")
+	r.Add(10, "foo")
+	r.Add(10, "foo")
+
+	s := r.mergeSorted()
+	if assert.Len(t, s.Rows, 2) {
+		assert.Equal(t, "bar", s.Rows[0].Groups[0])
+		assert.Equal(t, "foo", s.Rows[1].Groups[0])
+
+		assert.Equal(t, int64(24), s.Rows[0].Value)
+		assert.Equal(t, int64(30), s.Rows[1].Value)
+	}
+}
+
+func TestRecombine(t *testing.T) {
+	r := ResultsTable{Metric: "foo", Groups: []string{"Gate", "Status", "Enabled"}}
+	r.Add(5, "1", "activated", "true")
+	r.Add(3, "1", "in_progress", "true")
+	r.Add(8, "1", "activated", "false")
+	r.Add(2, "2", "investigation", "true")
+	r.Add(2, "2", "activated", "true")
+	r.Add(6, "1", "activated", "true")
+
+	if recombined := r.Recombine(); len(recombined) == 7 {
+		if len(recombined[0].Rows) == 4 {
+			assert.Equal(t, ResultsRow{Value: 8, Groups: []string{"activated", "false"}}, recombined[0].Rows[0])
+			assert.Equal(t, ResultsRow{Value: 13, Groups: []string{"activated", "true"}}, recombined[0].Rows[1])
+			assert.Equal(t, ResultsRow{Value: 3, Groups: []string{"in_progress", "true"}}, recombined[0].Rows[2])
+			assert.Equal(t, ResultsRow{Value: 2, Groups: []string{"investigation", "true"}}, recombined[0].Rows[3])
+		}
+
+		if len(recombined[1].Rows) == 2 {
+			assert.Equal(t, ResultsRow{Value: 8, Groups: []string{"false"}}, recombined[1].Rows[0])
+			assert.Equal(t, ResultsRow{Value: 18, Groups: []string{"true"}}, recombined[1].Rows[1])
+		}
+
+		if len(recombined[2].Rows) == 1 {
+			assert.Equal(t, ResultsRow{Value: 26}, recombined[2].Rows[0])
+		}
+
+		if len(recombined[3].Rows) == 3 {
+			assert.Equal(t, ResultsRow{Value: 21, Groups: []string{"activated"}}, recombined[3].Rows[0])
+			assert.Equal(t, ResultsRow{Value: 3, Groups: []string{"in_progress"}}, recombined[3].Rows[1])
+			assert.Equal(t, ResultsRow{Value: 2, Groups: []string{"investigation"}}, recombined[3].Rows[2])
+		}
+
+		if len(recombined[4].Rows) == 3 {
+			assert.Equal(t, ResultsRow{Value: 8, Groups: []string{"1", "false"}}, recombined[4].Rows[0])
+			assert.Equal(t, ResultsRow{Value: 14, Groups: []string{"1", "true"}}, recombined[4].Rows[1])
+			assert.Equal(t, ResultsRow{Value: 4, Groups: []string{"2", "true"}}, recombined[4].Rows[2])
+		}
+
+		if len(recombined[5].Rows) == 2 {
+			assert.Equal(t, ResultsRow{Value: 22, Groups: []string{"1"}}, recombined[5].Rows[0])
+			assert.Equal(t, ResultsRow{Value: 4, Groups: []string{"2"}}, recombined[5].Rows[1])
+		}
+
+		if len(recombined[6].Rows) == 4 {
+			assert.Equal(t, ResultsRow{Value: 19, Groups: []string{"1", "activated"}}, recombined[6].Rows[0])
+			assert.Equal(t, ResultsRow{Value: 3, Groups: []string{"1", "in_progress"}}, recombined[6].Rows[1])
+			assert.Equal(t, ResultsRow{Value: 2, Groups: []string{"2", "activated"}}, recombined[6].Rows[2])
+			assert.Equal(t, ResultsRow{Value: 2, Groups: []string{"2", "investigation"}}, recombined[6].Rows[3])
+		}
+	}
+}
